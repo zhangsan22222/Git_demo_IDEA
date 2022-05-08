86a5e6ef4559fb1f348fe916474d32dfd920bf0d
K <2510268647@qq.com>
2022-05-08 20:03:13 +0800

    IDEA first commit

---
+++ src/main/java/com/atguigu/Git/Payment.java
@@ -0,0 +1,11 @@
+package com.atguigu.Git;
+
+/**
+ * @author K
+ * @create 2022-05-08-20:00
+ */
+public class Payment {
+    public static void main(String[] args) {
+        System.out.println("这是我创建的东西");
+    }
+}
